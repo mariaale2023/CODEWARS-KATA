1c6304f97d29079b1f8b1e30ba19852e75b51964
mariaale2023 <123910242+mariaale2023@users.noreply.github.com>
2023-03-24 12:07:17 +1300

    creat a new folder

---
+++ CODEWARS/6kyu/test/test.js
@@ -0,0 +1,77 @@
+"use strict";
+
+function isValidWalk(walk) {
+  // How to get the count of each letter
+
+  console.log(walk);
+  console.log(walk.length);
+
+  const numberN = walk.reduce((acc, letter) => {
+    if (letter === "n") {
+      return acc + 1;
+    } else {
+      return acc;
+    }
+  }, 0);
+
+  const numberW = walk.reduce((acc, letter) => {
+    if (letter === "w") {
+      return acc + 1;
+    } else {
+      return acc;
+    }
+  }, 0);
+
+  const numberS = walk.reduce((acc, letter) => {
+    if (letter === "s") {
+      return acc + 1;
+    } else {
+      return acc;
+    }
+  }, 0);
+
+  const numberE = walk.reduce((acc, letter) => {
+    if (letter === "e") {
+      return acc + 1;
+    } else {
+      return acc;
+    }
+  }, 0);
+
+  console.log(numberN);
+  console.log(numberS);
+  console.log(numberW);
+  console.log(numberE);
+
+  let validWalk =
+    walk.length === 10 && numberN - numberS === 0 && numberW - numberE === 0
+      ? true
+      : false;
+  return validWalk;
+
+  // if (walk.length !== 10){
+  //     return false
+  //   } else if (numberN - numberS !== 0)
+  //   {
+  //     return false
+  //   } else if (numberW - numberE !== 0)
+  //   {
+  //     return false
+  //   }   else {
+  //     return true
+  //   }
+}
+
+console.log(isValidWalk(["n", "s", "n", "s", "n", "s", "n", "s", "n", "s"]));
+
+console.log(
+  isValidWalk(["w", "e", "w", "e", "w", "e", "w", "e", "w", "e", "w", "e"])
+);
+console.log(isValidWalk(["w"]));
+console.log(isValidWalk(["n", "n", "n", "s", "n", "s", "n", "s", "n", "s"]));
+
+const myArray = ["w", "e", "w", "e", "w", "e", "w", "e", "w", "e", "w", "e"];
+
+// How to get the count of each letter
+let amor;
+// how to get the values from the object
